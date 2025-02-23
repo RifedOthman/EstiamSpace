9a2d3b9c5c617880055a41793d0d4c8cba9a5779
rifedothman <rifed.othmen@gmail.com>
2025-02-23 15:41:06 +0100

    changes

---
+++ constants/index.js
@@ -0,0 +1,3 @@
+
+export const supabaseUrl = 'https://gamzjdoabdgiucnklpto.supabase.co';
+export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhbXpqZG9hYmRnaXVjbmtscHRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwNjU4NjUsImV4cCI6MjA1NTY0MTg2NX0.gtjtRrRodTDrr_goOBWNzhzLcLoVAgj1OUjeRfhtDnY';
